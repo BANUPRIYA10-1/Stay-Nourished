0cdbc304105612a2992c81a508f473989f67d525
BANUPRIYA K <76400807+BANUPRIYA10-1@users.noreply.github.com>
2021-07-11 21:43:36 +0530

    Project Urls.py
    Added Project urls.py File

--- urls.py
+++ urls.py
@@ -1,20 +1,25 @@
-from django.urls import path
-from .import views
+"""Dummy_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns=[
-    path("", views.first, name='firsts'),
-    path("logins", views.login, name='login'),
-    path("registers",views.register,name='register'),
-    path("home",views.home1,name='home'),
-    path("about",views.about,name='about'),
-    path("typesofdiet",views.typesofdiet,name='typesofdiet'),
-    path("tellusaboutyou1",views.tellusaboutyou1,name='tellusaboutyou1'),
-    path("tellusaboutyou",views.tellusaboutyou,name='tellusaboutyou'),
-    path("logout",views.logout,name='logout'),
-    path("knowyourfoods",views.knowyourfoods,name='knowyourfoods'),
-    path("contactus",views.contactus,name='contactus'),
-    path("gallery",views.gallery,name='gallery'),
 
+urlpatterns = [
+    path('', include('Dummy_app.urls')),
+    path('admin/', admin.site.urls),
 ]
-
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
